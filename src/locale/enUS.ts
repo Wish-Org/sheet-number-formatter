b1b8678eb42c0468df32bcfced0117ab8d39eb8c
Greg <gregory@betterreports.com>
2026-05-12 11:34:12 +1000

    feat: add SheetLocale interface and enUS built-in

---
+++ src/locale/enUS.ts
@@ -0,0 +1,14 @@
+import type { SheetLocale } from "./types.js";
+
+export const enUS: SheetLocale = {
+  decimalSeparator: ".",
+  groupSeparator: ",",
+  dateSeparator: "/",
+  monthNames: ["January","February","March","April","May","June","July","August","September","October","November","December"],
+  shortMonthNames: ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
+  singleLetterMonthNames: ["J","F","M","A","M","J","J","A","S","O","N","D"],
+  dayNames: ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
+  shortDayNames: ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],
+  amLabel: "AM",
+  pmLabel: "PM",
+};
